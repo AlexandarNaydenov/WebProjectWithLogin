3579e828df84800b6f955f4b51dacf8e0b0000ed
Alexandar Naydenov <alexandar.naydenov99@gmail.com>
2021-06-12 11:29:09 +0300

    add export to csv

---
+++ WebProject/js/exportRecordsIntoCSV.js
@@ -0,0 +1,29 @@
+function exportRecordsIntoCSV(){
+    fetch('http://localhost/WebProjectFinal/WebProject/php/getRecords.php')
+        .then(response => response.json())
+        .then(data => downloadRecords(data))
+        .catch(function(error) {
+        console.log('Request failed', error);
+    })
+}
+
+
+
+function downloadRecords(records){
+
+    let csvContent = "data:text/csv;charset=utf-8,";
+
+    records.forEach(function (element){
+        let row = element.id + ";" + element.record_date + ";" + element.record_starting_time + ";" +element.record_ending_time + ";" +element.is_break + ";" +element.student_name + ";" +element.student_fn + ";" +element.presentation_topic + ";" +element.presentation_category + ";" +element.password + ";" +element.lector_notes + ";" +element.lector_grade;
+        csvContent += row + "\r\n";
+    })
+
+
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "records.csv");
+    document.body.appendChild(link);
+
+    link.click();
+}
